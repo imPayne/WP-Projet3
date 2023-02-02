cc6f4c3758e525d7e1e90e70f8e55a454c938aea
imPayne <hugo.monchablon@epitech.eu>
2023-02-02 15:54:59 +0100

    :lipstick: added style to front-page header pink block

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,15 @@
 module.exports = {
   content: ["./**/*.php"],
   theme: {
-    extend: {},
-    colors: {
-        "pink": "#F17872"
-    }
+    extend: {
+        colors: {
+            "pink-light": "#EF6480",
+            "pink-strong": "#F03B5E"
+        },
+        fontFamily: {
+            "nunito": ['"Nunito Sans"']
+        }
+    },
   },
   plugins: [],
 }
