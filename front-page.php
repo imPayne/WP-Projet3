3b020349bfa13752b2a284a75d8c89a9f0485160
imPayne <hugo.monchablon@epitech.eu>
2023-02-02 14:45:42 +0100

    :hammer: First Commit, added .gitignore, script php, tailwind config file added pink custom color, added front-page

---
+++ front-page.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?>
+
+<div class="w-full h-96 bg-pink">
+    <h1 class="">
+        <?php
+        the_field('title');
+        ?>
+    </h1>
+    <h2>
+        <?php
+            the_field('subtitle');
+        ?>
+    </h2>
+    <a href="/">
+        <?php 
+            the_field('buttontext');
+        ?>
+    </a>
+    <a href="/">
+        <?php 
+            the_field('buttoncontact');
+        ?>
+    </a>
+</div>
+
+<?php get_footer(); ?>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.php"],
+  theme: {
+    extend: {},
+    colors: {
+        "pink": "#F17872"
+    }
+  },
+  plugins: [],
+}
