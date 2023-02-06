e8a55a4bb3d4559a54382c48912ebff02cd83fed
imPayne <hugo.monchablon@epitech.eu>
2023-02-06 18:43:40 +0100

    added front-page style, fix hierarchie and shit code in acf fields

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 add_image_size( 'section2ImageSize', 232, 170, array( "center", "center" ) );
-add_image_size( 'section3ImageSize', 426, 161, array( "center", "center" ) );
+add_image_size( 'section3ImageSize', 429, 165, array( "center", "center" ) );
 add_image_size( 'section4ImageSize', 171, 171, array( "center", "center" ) );
 add_image_size( 'section5ImageSize', 409, 230, array( "center", "center" ) );
 add_image_size( 'section6ImageSize', 250, 50, array( "center", "center" ) );
